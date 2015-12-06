ebdde24eb54c29a8852bb52abc5c16b75e0ab989
njd5475 <njd5475@gmail.com>
2015-12-06 18:39:02 -0500

    Tests now pass, basic implementation achieved.
    Now take over the world.

--- src/main/java/com/njd5475/deckofcards/Dealable.java
+++ src/main/java/com/njd5475/deckofcards/Dealable.java
@@ -1,10 +1,33 @@
 package com.njd5475.deckofcards;
 
+/**
+ * This interface defines the operations necessary to be a dealable deck of cards. A dealable deck
+ * is only meant to deal out cards and is not meant to have cards returned to the deck.
+ * 
+ * @author njd5475
+ */
 public interface Dealable {
 
+	/**
+	 * This method randomizes the remaining cards
+	 */
 	public void shuffle();
 	
+	/**
+	 * Returns a card if there are more cards to be dealt. Otherwise this method returns null.
+	 * <br/>
+	 * This method never returns the same card more than once.
+	 * 
+	 * @return A card instance if more to be dealt
+	 */
 	public Card dealOneCard();
 	
+	/**
+	 * Returns the top card on the deck if there is one. Or null otherwise.
+	 * <br/>
+	 * This method will return the same card whenever called until dealOneCard or shuffle are called.
+	 * 
+	 * @return Top card.
+	 */
 	public Card peekTopCard();
 }
--- src/main/java/com/njd5475/deckofcards/Deck.java
+++ src/main/java/com/njd5475/deckofcards/Deck.java
@@ -1,32 +1,80 @@
 package com.njd5475.deckofcards;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
+/**
+ * This is a simple implementation of a dealable deck of cards. The deck is
+ * defined by a set of cards which are created from an
+ */
 public class Deck implements Dealable {
 
-	private Card cards[];
-	private Random random;
+	private List<Card>	cards;
+	private Random		random;
 
+	/**
+	 * Creates a deck of cards using the suits and faces to construct a list of
+	 * cards. 
+	 * 
+	 * @param suits The suits to be used
+	 * @param faces The faces for each suit
+	 * @param random A random number generator.
+	 */
 	public Deck(Suit suits[], Face faces[], Random random) {
-		cards = new Card[suits.length * faces.length];
-		int i = 0;
+		cards = new ArrayList<Card>(suits.length * faces.length);
 		for (Suit suit : suits) {
 			for (Face face : faces) {
-				cards[i++] = new Card(suit, face);
+				cards.add(new Card(suit, face));
 			}
 		}
 		this.random = random;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.njd5475.deckofcards.Dealable#shuffle()
+	 */
 	public void shuffle() {
+		Card[] array = cards.toArray(new Card[cards.size()]);
+		Card current = null;
+		int replacedLocation = 0;
+		// iterate over entire array once
+		for (int i = 0; i < array.length; ++i) {
+			current = array[i];
+			replacedLocation = random.nextInt(array.length);
 
+			// swap with random location anywhere in array
+			array[i] = array[replacedLocation];
+			array[replacedLocation] = current;
+		}
+
+		cards = new ArrayList<Card>(Arrays.asList(array));
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.njd5475.deckofcards.Dealable#dealOneCard()
+	 */
 	public Card dealOneCard() {
+		if (!cards.isEmpty()) {
+			return cards.remove(0);
+		}
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.njd5475.deckofcards.Dealable#peekTopCard()
+	 */
 	public Card peekTopCard() {
+		if (!cards.isEmpty()) {
+			return cards.get(0);
+		}
 		return null;
 	}
 }
