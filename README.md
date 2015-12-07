9f17acd238121124827724ccb123202a743468d7
njd5475 <njd5475@gmail.com>
2015-12-06 20:11:28 -0500

    Added some additional documentation and a different deck type check.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # deckofcards
 Library to simulate a regular 52 card deck of playing cards.
 
+# How to use
+Simply instantiate a new Deck using either the enums Suit, and Face or build arrays made up of how many combinations of each Suit and Face card   for different variations of playing card decks. 
+
+		// For instance, a pinochle deck
+		Deck = new Deck(Suit.values(), new Face[]{Ace, King, Queen, Jack, Ten, Nine, Ace, King, Queen, Jack, Ten, Nine});
+
+# Limitations and Future implementation
+The library currently only defines a Dealable deck. There are no capabilities for returning cards to a Dealable implementation. There migh be an additional implementation of a Redealable deck. Possibly creating an interface for a discard pile, something like ReplaceableDeck. Or make another Dealable implementation that recreates the deck with subsequent call to shuffle after all cards have been dealt.
--- src/test/java/com/njd5475/deckofcards/DeckTest.java
+++ src/test/java/com/njd5475/deckofcards/DeckTest.java
@@ -1,5 +1,11 @@
 package com.njd5475.deckofcards;
 
+import static com.njd5475.deckofcards.Face.Ace;
+import static com.njd5475.deckofcards.Face.Jack;
+import static com.njd5475.deckofcards.Face.King;
+import static com.njd5475.deckofcards.Face.Nine;
+import static com.njd5475.deckofcards.Face.Queen;
+import static com.njd5475.deckofcards.Face.Ten;
 import static org.junit.Assert.*;
 
 import java.util.Random;
@@ -15,7 +21,7 @@ public class DeckTest {
 	public void setup() {
 		deck = new Deck(Suit.values(), Face.values(), new Random(System.currentTimeMillis()));
 	}
-	
+
 	@Test
 	public void testShuffle() throws Exception {
 		Card topBeforeSuffle = deck.peekTopCard();
@@ -28,7 +34,7 @@ public class DeckTest {
 	@Test
 	public void testDealOneCard() throws Exception {
 		deck.shuffle();
-		for(int i = 0; i < 52; ++i) {
+		for (int i = 0; i < 52; ++i) {
 			assertNotNull(deck.dealOneCard());
 		}
 
@@ -37,4 +43,17 @@ public class DeckTest {
 		assertNull(deck.peekTopCard());
 	}
 
+	@Test
+	public void testCreationOfPinochleDeck() {
+		// For instance, a pinochle deck
+		Deck pinochleDeck = new Deck(Suit.values(),
+				new Face[] { Ace, King, Queen, Jack, Ten, Nine, Ace, King, Queen, Jack, Ten, Nine },
+				new Random(System.currentTimeMillis()));
+
+		pinochleDeck.shuffle();
+		for (int i = 0; i < 48; ++i) {
+			assertNotNull(pinochleDeck.dealOneCard());
+		}
+		assertNull(pinochleDeck.dealOneCard());
+	}
 }
